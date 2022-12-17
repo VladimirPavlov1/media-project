c6d78175c084347525b1717ddc29d43bdd295249
VladimirPavlov1 <106485364+VladimirPavlov1@users.noreply.github.com>
2022-12-17 21:03:03 +0000

    Deploying to gh-pages from @ VladimirPavlov1/media-project@75527223383f2a8452c729ba7c20f601c977a2df 🚀

---
+++ index.4a24887b.js
@@ -0,0 +1,2 @@
+fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=27cddf1230eab22bdf20f54c99a70037").then((function(e){return e.json()})).then((function(e){return console.log(e.results)}));
+//# sourceMappingURL=index.4a24887b.js.map
