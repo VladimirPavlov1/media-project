b9abef4bed81a3aeb6625c6664f16702f835c078
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2022-12-19 18:43:47 +0200

    form add class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const formEl=document.querySelector('.form-search');
+console.log(formEl)
+
+
 function createCardMovie(arr){
     return arr.map(item=>`<li class="gallery__item">
     <img src="${item.poster_path}" alt="${item.title}">
@@ -10,3 +14,4 @@ function createCardMovie(arr){
 fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=27cddf1230eab22bdf20f54c99a70037")
 .then(response=>response.json())
 .then(data=>console.log(data.results))
+
